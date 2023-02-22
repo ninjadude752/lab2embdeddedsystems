4beba0bee0446205be3b77b470460fee6a64c144
Spencer Blackwell <spencerblackwell02@gmail.com>
2023-02-22 12:53:08 -0600

    Update main.asm

--- main.asm
+++ main.asm
@@ -10,7 +10,7 @@ sbi DDRB,3 ;set PB3 output (pin 11 on board) (14 on Counter)
 sbi DDRB,2 ;set PB2 output (pin 10 on board) (pin 11 on counter)
 sbi DDRB,1 ;set PB1 output (pin 9 on board) (pin 13 on counter)
 cbi DDRB,0 ;set PB0 input (pin 8) (to upper left on button A)
-cbi DDRD,7 ;set PD7 input, pin 7, on upper left button B
+cbi DDRB,4 ;set PD7 input, pin 7, on upper left button B
 
 .equ count = 0xfff		; assign a 16-bit value to symbol "count"
 .equ fourth_count = 0xff1 
@@ -26,9 +26,11 @@ cbi DDRD,7 ;set PD7 input, pin 7, on upper left button B
 .equ disp7 = 0x7
 .equ disp8 = 0x7F
 .equ disp9 = 0x6F
+.equ dispFlash = 0x80
 
 
 ldi R16,0x00
+ldi R23, 0
 ldi R21, 0x00
 ldi R18,disp0
 
@@ -39,6 +41,9 @@ start:
 
 SBIs PINB,0
 rjmp press
+
+SBIS PINB,4
+rjmp countdown
 
 ;SBIC PIND,7
 ;rjmp countdown
@@ -55,6 +60,7 @@ rjmp start
 press:
 	;rcall delay_long
 	;rcall countA
+	clr R23 
 	ldi r25, 20
 	loop3:
 
@@ -95,7 +101,8 @@ press:
 	;go into countA, and inc, then call a delay.
 	;if the counter is 4 or less, only inc R16 by 1, otherwise, reset R16
 
-
+	midway:
+	rjmp start
 
 
 display: ; backup used registers on stack
@@ -160,14 +167,22 @@ count_to_digital:
 	breq check_8
 	cpi R16, 0x09
 	breq check_9
-	ret
+	cpi r16, 9
+	brge midway
 	check_0:
 		clr R18
 		ldi R18, disp0
 
-		sbic PINB, 0
-		rjmp start
-		rjmp check_0
+		cpi R23, 1
+		breq check_0
+		
+		cpi R23,0
+		breq midway
+
+		;sbic PINB, 0
+		;rjmp start
+		;rjmp check_0
+
 	check_1:
 		clr R18
 		ldi R18, disp1
@@ -204,7 +219,6 @@ count_to_digital:
 		clr R18
 		ldi R18, disp9
 		rjmp start
-
 
 ;loop:
 ;sbi   PORTB,1     ; LED at PB1 off
@@ -249,17 +263,38 @@ d2:
 	
 
 countdown:
-	tst R16
-	breq flash
-	tst R16
-	brne down_one
-
-	flash:
-
+	loop2:
+		inc R23
+		dec r16
+		rjmp count_to_digital
 		
 
+/*	flash:
+	ldi R22, 4 
+
+	loopFlash:
+		rcall delay_long
+		rcall delay_long
+		rcall delay_long
+		rcall delay_long
+		ldi R16, dispFlash
+		rjmp count_to_digital
+		dec R22
+		brne loopFlash
+	
+
 	down_one:
+		ldi R24, 20
+
+
+		loopSec:
 		rcall delay_long
 		dec R16
-		rcall count_to_digital
+		rjmp count_to_digital
+		dec r24
+		brne loopSec
+
+		rjmp flash*/
+
 		rjmp start
+
